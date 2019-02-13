8488b2affa937e989c91fdca40ae3b141f68166f
BoxHen <hchen101@binghamton.edu>
2019-02-12 23:18:55 -0500

    infrared test

---
+++ Rover/infrared_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as IO
+IO.setwarnings(False)
+IO.setmode (IO.BCM)
+
+IO.setup(2,IO.OUT) #GPIO 2 - LED as output
+IO.setup(14,IO.IN) #GPIO 14 - IR sensor input
+
+While 1:
+	if(IO.input(14)==True): #No object detected 
+		IO.output(2,False) #led OFF
+
+	if(IO.input(14)==False): #object detected
+		IO.output(2,True) #led ON
