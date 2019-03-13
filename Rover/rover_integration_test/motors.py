2beec5db0c06022546841099f96cae41c1b4bafe
BoxHen <hchen101@binghamton.edu>
2019-03-13 02:43:44 -0400

    motor node complete

--- Rover/rover_integration_test/motors.py
+++ Rover/rover_integration_test/motors.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python2
 
 import rospy
+import motor_move
 from std_msgs.msg import String
 
-def callback(data):
+def move_rover(data):
+	speed = 30
 	rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-	
-    
+	rover_action = data.data
+	if rover_action == "RIGHT":    
+		motor_move.right(speed)
+	elif rover_action == "LEFT":  
+		motor_move.left(speed)
+	elif rover_action == "FORWARD": 	
+		motor_move.forward(speed)
+	else:#rover_action == "BACKWARD"   	
+		motor_move.backward(speed)
+
 def listener():
 	rospy.init_node('motor', anonymous=True)
-	rospy.Subscriber("setMotor", Int16MultiArray, callback)
+	rospy.Subscriber("setMotor", String, move_rover)
 	rospy.spin()
 
 if __name__ == '__main__':
