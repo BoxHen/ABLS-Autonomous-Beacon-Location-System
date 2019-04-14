80352cc6227454d3241bad6bc4edfdac031efba2
BoxHen <hchen101@binghamton.edu>
2019-04-14 16:15:05 -0400

    unit test for ultrasonic pub

--- Rover/test/refactored_code/unit test/test_ultrasonic_pub.py
+++ Rover/test/refactored_code/unit test/test_ultrasonic_pub.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env/ python2
 import rospy
 import time
+import RPi.GPIO as GPIO
+GPIO.setmode (GPIO.BCM)
 from std_msgs.msg import Int16
 from std_msgs.msg import Int16MultiArray 
 from ultrasonic_sensor import ultrasonic_sensor
 from ultrasonic_threshold import get_threshold_flag
 
 class ultrasonics_on_rover():
-    def __init__(self, number_of_sensors, pins)
+    def __init__(self, number_of_sensors, pins):
         self.distance_pub = rospy.Publisher('isObstacle', Int16MultiArray, queue_size=1)
-	    self.flag_pub = rospy.Publisher('isFlagSet', Int16, queue_size=1)
+        self.flag_pub = rospy.Publisher('isFlagSet', Int16, queue_size=1)
         rospy.init_node('ultrasonic_sensor', anonymous = True)
         self.number_of_sensors = number_of_sensors
         self.pins = pins
         self.sensors = []
-        self.distances = Int16MultiArray()
+        self.distancesultrasonics_on_rover = Int16MultiArray()
         self.threshold_flag = 0
         
 
@@ -44,3 +46,13 @@ class ultrasonics_on_rover():
             self.flag_pub.publish(self.threshold_flag)
             print("sensor:", sensor_reading_array.data)
             print("flags:", threshold_flag)
+            
+if __name__ == '__main__':
+    #rospy.init_node('algorithm', anonymous=True)
+    try:
+        pins = [2,3,4,17,27,22,10,9]
+        ultrasonics_on_rover = ultrasonics_on_rover(4, pins)
+        ultrasonics_on_rover.create_sensors()
+        print(ultrasonics_on_rover.sensors)
+    except rospy.ROSInterruptException:  
+        pass
