6e95113c94484b8feba2741a63af7f3bb9bf5756
BoxHen <hchen101@binghamton.edu>
2019-04-17 16:17:28 -0400

    swapped forward and backward command due to motor failure

--- Rover/Motor/import/command.py
+++ Rover/Motor/import/command.py
@@ -1,11 +1,12 @@
+#I have swapped forward and backwards due to one motor failure
 # Right motor commands for TRex Motor
-RIGHT_MOTOR_BACKWARD = 0xC9
-RIGHT_MOTOR_FORWARD = 0xCA
+RIGHT_MOTOR_BACKWARD = 0xCA #0xC9 - offcial forward
+RIGHT_MOTOR_FORWARD = 0xC9 #0xCA - offcial backward
 RIGHT_MOTOR_BRAKE = 0xC8
 
 # Left motor commands for TRex Motor
-LEFT_MOTOR_BACKWARD = 0xC1
-LEFT_MOTOR_FORWARD = 0xC2
+LEFT_MOTOR_BACKWARD = 0xC2 #0xC1 - offcial forward
+LEFT_MOTOR_FORWARD = 0xC1 #0xC2 - offcial backward
 LEFT_MOTOR_BRAKE = 0xC0
 
 
