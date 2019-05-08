07f5e15c247358e931df304fc6a554230d1b05a4
BoxHen <hchen101@binghamton.edu>
2019-05-07 20:06:50 -0400

    attempt at beacon pub fix

--- Rover/compass/heading_pub.py
+++ Rover/compass/heading_pub.py
@@ -1,19 +1,18 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 import time
 import math
-import rospy
 import board
 import busio
+import struct
+import socket
 import Adafruit_LSM303
-
-from std_msgs.msg import Int16MultiArray 
-from std_msgs.msg import Int16
 
 class heading:
 	def __init__(self):
 		self.heading = 0 #compass heading in degrees
-		self.heading_pub = rospy.Publisher('compass_heading', Int16, queue_size=1)
-		rospy.init_node('compass_heading', anonymous = True)
+		self.s = socket.socket() # Create a socket object
+		self.host = socket.gethostname() # Get local machine name
+		self.port = 12345                # Reserve a port for your service.
 
 		self.lsm303 = Adafruit_LSM303.LSM303() # Create a LSM303 instance
 		print('Printing accelerometer & magnetometer X, Y, Z axis values, press Ctrl-C to quit...')
@@ -36,6 +35,19 @@ class heading:
 
 			# Wait half a second and repeat.
 			time.sleep(0.5)
+
+	def socket(self):
+		self.s.bind((self.host, self.port))        # Bind to the port
+
+		self.s.listen(5)                 # Now wait for client connection.
+		while True:
+			c, addr = self.s.accept()     # Establish connection with client.
+			print ('Got connection from', addr)
+			#c.send('Thank you for connecting'.encode())
+			heading = struct.pack('f', self.heading)
+			c.sendall(heading)
+			c.close()                # Close the connection
+
 
 if __name__ == '__main__':
  	try:
